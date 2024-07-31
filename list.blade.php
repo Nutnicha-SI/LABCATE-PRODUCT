cd6f5c86d7960c9618fed52653478c6a86c325d2
Nicha <nichastc01@gmail.com>
2024-07-31 16:05:40 +0700

    Add files via upload

--- list.blade.php
+++ list.blade.php
@@ -1,40 +1,18 @@
+<!-- resources/views/categories/list.blade.php -->
 @extends('layouts.main')
 
 @section('title', $title)
 
 @section('content')
-
 <main>
-    <form action="{{ route('products.list') }}" method="get">
-        
+    <form action="{{ route('categories.list') }}" method="get">
         <div class="search">
-            <label for="app-inp-search">Search ::</label>
-            <input id="app-inp-search" type="text" name="term" value="{{ $term }}" />
-            <br>
+            <label for="cmp-search">Search ::  </label>
+            <input id="cmp-search" type="text" name="term" value="{{ $term }}" />
             <div class="button-bt">
                 <button type="submit">Submit</button>
             </div>
         </div>
-
-        <br>
-        <table style="width: 100%; text-align: center;">
-            <tr>
-                <td>
-                    <div class="price-range">
-                        <label for="min-price">Min Price ::</label>
-                        <input align-item="center" id="min-price" type="number" name="min_price" value="{{ $min_price }}" step="0.01" />
-                        <br>
-                        <br><label for="max-price">Max Price ::</label>
-                        <input id="max-price" type="number" name="max_price" value="{{ $max_price }}" step="0.01" />
-                    </div>
-                </td>
-            </tr>
-        </table>
-        <br>
-        <div class="button-bt">
-            <button type="submit">Submit</button>
-        </div>
-        <br>
     </form>
     <br>
     <table class="app-cmp-data-list">
@@ -42,27 +20,29 @@
             <tr>
                 <th>Code</th>
                 <th>Name</th>
-                <th>Categories</th>
-                <th>Price</th>
             </tr>
         </thead>
         <tbody>
-        @foreach($products as $product)
-            <tr>
-                <td>{{ $product['code'] }}</td> 
-                <td>{{ $product['name'] }}</td>
-                <td> 
-                    @foreach($product['categories'] as $category) 
-                    <a href="{{ route('categories.view', $category['code']) }}" class="category-box">
-                        {{ $category['name'] }}
+            
+            @foreach($categories as $category)
+            <a href="{{ route('categories.view', $category['code']) }}" ></a>
+                <tr>
+                    <td><a href="{{ route('categories.view', $category['code']) }}" class="category-box">
+                        {{ $category['code'] }}
+                    </a></td>
+                    <td>
+                        <a {{ route('categories.view', $category['code']) }}>
+                            {{ $category['name'] }}
                         </a>
-                    @endforeach
-                </td>
-                <td class="number">{{ number_format($product['price'], 2) }}</td>
-            </tr>
-        @endforeach
+                        
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
-</main>
-
+</main>  
 @endsection
+<td>
+   
+    
+</td>
